01154e41c5bc982c9021c6032e5d1fd843ae44c3
Mujayit Abdu <mujiayitiabdu@gmail.com>
2024-08-29 19:53:20 -0400

    reverseString

---
+++ src/test/java/coding/practice/hackerrank/ReverseString.java
@@ -0,0 +1,26 @@
+package coding.practice.hackerrank;
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+
+		System.out.println(reversedStringWithStringBuilder("John"));
+
+	}
+
+	public static String reversedStringWithStringBuilder(String input) {
+
+		char[] stringToChar = input.toCharArray();
+
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = stringToChar.length - 1; i >= 0; i--) {
+
+			sb.append(stringToChar[i]);
+
+		}
+
+		return sb.toString();
+	}
+
+}
