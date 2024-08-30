61b6375b9352bd8bba20b2e0584ec4ed081ac551
Mujayit Abdu <mujiayitiabdu@gmail.com>
2024-08-30 11:10:39 -0400

    fibtill nth is added

--- src/test/java/coding/practice/hackerrank/FibonacciSeries.java
+++ src/test/java/coding/practice/hackerrank/FibonacciSeries.java
@@ -4,44 +4,43 @@ public class FibonacciSeries {
 
 	public static void main(String[] args) {
 
-		try {
-
-//			System.out.println(nthFib(6));
-
-			System.out.println(nthFib(6));
-		} catch (IllegalArgumentException e) {
-			System.err.print("Exception: " + e.getMessage());
-		}
+		fibTillNth(10);
 
 	}
 
 	// Write a Java program for generating the nth Fibonacci Number using loop
 
-//	public static int nthFib(int nth) {
-//
-//		if (nth < 0) {
-//			throw new IllegalArgumentException("Inpust must be a non-negative integer.");
-//		}
-//
-//		int a = 0;
-//		int b = 1;
-//
-//		if (nth == 0) {
-//			return a;
-//		} else if (nth == 1) {
-//			return b;
-//		}
-//
-//		for (int i = 2; i <= nth; i++) {
-//			int next = a + b;
-//			a = b;
-//			b = next;
-//		}
-//
-//		return b;
-//	}
+	public static void fibTillNth(int nthFib) {
+		for (int i = 0; i <= nthFib; i++) {
+			System.out.print(nthFibRecursive(i) + " ");
+		}
+	}
 
 	public static int nthFib(int nth) {
+
+		if (nth < 0) {
+			throw new IllegalArgumentException("Inpust must be a non-negative integer.");
+		}
+
+		int a = 0;
+		int b = 1;
+
+		if (nth == 0) {
+			return a;
+		} else if (nth == 1) {
+			return b;
+		}
+
+		for (int i = 2; i <= nth; i++) {
+			int next = a + b;
+			a = b;
+			b = next;
+		}
+
+		return b;
+	}
+
+	public static int nthFibRecursive(int nth) {
 
 		if (nth < 0) {
 			throw new IllegalArgumentException("input must be non-negative integar.");
