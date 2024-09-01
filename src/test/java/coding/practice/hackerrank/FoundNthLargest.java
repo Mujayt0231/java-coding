e9cdd26f69b77a339e250432f7d13827cfecfd14
Mujayit Abdu <mujiayitiabdu@gmail.com>
2024-09-01 13:54:22 -0400

    test

---
+++ src/test/java/coding/practice/hackerrank/FoundNthLargest.java
@@ -0,0 +1,16 @@
+package coding.practice.hackerrank;
+
+public class FoundNthLargest {
+
+	public static void main(String[] args) {
+
+	}
+
+	public static int findLargestNum(int[] numArr) {
+
+		int firstNum = numArr[0];
+
+		return 0;
+	}
+
+}
