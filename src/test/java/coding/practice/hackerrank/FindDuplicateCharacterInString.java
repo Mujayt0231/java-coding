9ba2e0ae62881917aa952b07e808b560278641de
Mujayit Abdu <mujiayitiabdu@gmail.com>
2024-09-02 13:42:41 -0400

    find duplicate char practice

--- src/test/java/coding/practice/hackerrank/FindDuplicateCharacterInString.java
+++ src/test/java/coding/practice/hackerrank/FindDuplicateCharacterInString.java
@@ -8,6 +8,8 @@ public class FindDuplicateCharacterInString {
 
 	public static void main(String[] args) {
 		System.out.println(duplicateCharCount("JavaJ2EE"));
+		findDupchar("JavaJ2EE");
+
 	}
 
 	public static String duplicateCharCount(String input) {
@@ -31,6 +33,23 @@ public class FindDuplicateCharacterInString {
 		}
 
 		return sb.toString();
+	}
+
+	public static void findDupchar(String input) {
+
+		char[] stringAschar = input.toCharArray();
+
+		Map<Character, Integer> map = new HashMap<Character, Integer>();
+
+		for (char c : stringAschar) {
+			map.put(c, map.getOrDefault(c, 0) + 1);
+		}
+
+		for (Entry<Character, Integer> entry : map.entrySet()) {
+			if (entry.getValue() > 1) {
+				System.out.println("character: " + entry.getKey() + "-----" + entry.getValue());
+			}
+		}
 	}
 
 }
