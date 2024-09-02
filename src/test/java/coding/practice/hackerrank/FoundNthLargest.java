85b3fff833f3af2718d2b41507e32a48a4b1b442
Mujayit Abdu <mujiayitiabdu@gmail.com>
2024-09-02 13:53:54 -0400

    found nth largest

--- src/test/java/coding/practice/hackerrank/FoundNthLargest.java
+++ src/test/java/coding/practice/hackerrank/FoundNthLargest.java
@@ -1,8 +1,23 @@
 package coding.practice.hackerrank;
 
+import java.util.Arrays;
+
 public class FoundNthLargest {
 
 	public static void main(String[] args) {
+
+		int[] array = { 4, 2, 3, 4, 1, 2, 5 };
+
+		System.out.println(foundnthlargest(array, 4));
+		;
+
+	}
+
+	public static int foundnthlargest(int[] num, int nthNum) {
+
+		int[] sortedArr = Arrays.stream(num).distinct().sorted().toArray();
+
+		return sortedArr[(sortedArr.length) - nthNum];
 
 	}
 
