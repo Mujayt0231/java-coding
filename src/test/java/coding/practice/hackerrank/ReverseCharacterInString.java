d93e8cc6d8eb7578d454b950610da0227ef21393
Mujayit Abdu <mujiayitiabdu@gmail.com>
2024-09-04 23:24:38 -0400

    reverseCharacter

--- src/test/java/coding/practice/hackerrank/ReverseCharacterInString.java
+++ src/test/java/coding/practice/hackerrank/ReverseCharacterInString.java
@@ -2,4 +2,35 @@ package coding.practice.hackerrank;
 
 public class ReverseCharacterInString {
 
+	public static void main(String[] args) {
+
+		System.out.println(reverseString("Test"));
+
+	}
+
+	public static String reverseString(String input) {
+
+		if (input == null || input.trim().isEmpty()) {
+			return "";
+		}
+
+		char[] array = input.toCharArray();
+
+		int left = 0;
+		int right = array.length - 1;
+
+		while (left < right) {
+
+			char temp = array[left];
+			array[left] = array[right];
+			array[right] = temp;
+			left++;
+			right--;
+
+		}
+
+		return new String(array);
+
+	}
+
 }
