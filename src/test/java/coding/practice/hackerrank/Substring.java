5e763deb4cbebc071ac7255473b6cbea0b3ff855
Mujayit Abdu <mujiayitiabdu@gmail.com>
2024-09-05 23:22:41 -0400

    chorse:substring

---
+++ src/test/java/coding/practice/hackerrank/Substring.java
@@ -0,0 +1,19 @@
+package coding.practice.hackerrank;
+
+public class Substring {
+
+	public static void main(String[] args) {
+
+		String name = "Hashimjon";
+
+		String subsname = name.substring(3);
+
+		System.out.println(name.substring(0, name.length() - 1));
+
+		System.out.println(subsname);
+
+		System.out.println(name.indexOf("Hashmuhemmed"));
+
+	}
+
+}
